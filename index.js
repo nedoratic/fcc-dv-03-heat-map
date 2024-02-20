82d04646428a8339d5d85c4052f7fd4064cbc230
nedoratic <nedoratic@gmail.com>
2024-02-20 11:24:05 +0100

    x and y axes added to canvas

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/m
 let req = new XMLHttpRequest();
 
 let baseTemperature;
-let monthlyVarianceValues;
+let monthlyVarianceValues = [];
 
 let xScale;
 let yScale;
@@ -15,3 +15,41 @@ let canvas = d3.select('#canvas');
 
 canvas.attr('height', height);
 canvas.attr('width', width);
+
+let generateScales = () => {
+	xScale = d3.scaleLinear().range([padding, width - padding]);
+	yScale = d3.scaleTime().range([padding, height - padding]);
+};
+
+let drawCells = () => {};
+
+let drawAxes = () => {
+	let xAxis = d3.axisBottom(xScale);
+	let yAxis = d3.axisLeft(yScale);
+
+	// Draw X Axis
+	canvas
+		.append('g')
+		.call(xAxis)
+		.attr('id', 'x-axis')
+		.attr('transform', `translate(0, ${height - padding})`);
+
+	// Draw Y Axis
+	canvas.append('g').call(yAxis).attr('id', 'y-axis').attr('transform', `translate(${padding}, 0)`);
+};
+
+// Fetching JSON Data
+req.open('GET', url, true);
+req.onload = () => {
+	// console.log(req.responseText);
+	let object = JSON.parse(req.responseText);
+	// console.log(object);
+	baseTemperature = object.baseTemperature;
+	// console.log(baseTemperature);
+	monthlyVarianceValues = object.monthlyVariance;
+	// console.log(monthlyVarianceValues);
+	generateScales();
+	drawCells();
+	drawAxes();
+};
+req.send();
